66f0a2306916a531e9eb2bc38fe855e24601d7f4
phillip-eth <54546971+phillip-eth@users.noreply.github.com>
2019-11-05 17:14:46 -0500

    message board

---
+++ messageBoard/server.js
@@ -0,0 +1,82 @@
+/////////// Setting Server & Dependencies ////////////
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { Schema }= mongoose;
+const port = process.env.PORT || 8000;
+const moment = require('moment');
+
+app.listen(port, ()=> console.log("listening on port: "+port));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/static"));
+app.set('views', __dirname+'/views');
+app.use(express.urlencoded({extended: true}));
+
+
+mongoose.connect('mongodb://localhost/message_board', 
+{useNewUrlParser:true,
+useUnifiedTopology:true,
+useCreateIndex: true}
+);
+
+mongoose.connection.once('connected', () => console.log("connected to Mongo DB"));
+
+////////////// DB Models ////////////
+
+const CommentSchema = new mongoose.Schema({
+    name:{type: String, required: [true,"Name must be at least 2 characters."], minlength: 2, trim: true},
+    text: {type:String, required: [true,"Message must be at least 5 characters."], minlength:5, trim: true},
+    message: {type: Schema.Types.ObjectId,
+    ref: "Message"}
+}, {timestamps:true }
+
+);
+
+
+const MessageSchema = new mongoose.Schema({
+    name:{type: String, required: [true,"Name must be at least 2 characters."], minlength: 2, trim: true},
+    text: {type:String, required: [true,"Message must be at least 10 characters."], minlength:10, trim: true},
+    comments: [CommentSchema],
+}, {timestamps:true }
+
+);
+
+const Comment = mongoose.model('Comment', CommentSchema);
+const Message = mongoose.model('Message', MessageSchema);
+
+//////////// Routes //////////////
+app.get('/', (req,resp)=>{
+    Message.find()
+        .then(data=> {
+            resp.render('index', {messages:data,moment:moment})
+        })
+        .catch(err=> err.throw(resp.json(err)));
+});
+
+app.post('/messages', (req,resp)=>{
+    Message.create(req.body)
+    .then(()=>{ resp.redirect('/')})
+    .catch(err => console.log(err));
+
+});
+
+app.post('/comments/new/:id', (req,resp)=>{
+    const newComment = new Comment();
+    newComment.name = req.body.name;
+    newComment.text = req.body.text;
+    newComment.save()
+    .then(
+        Message.findOneAndUpdate({_id:req.params.id}, {$push: {comments : newComment}}, (err)=>{
+        if(err){console.log(err);}
+        else{resp.redirect('/')}
+        
+    }))
+    .catch(
+        err => console.log(err)
+    );
+
+});
+
