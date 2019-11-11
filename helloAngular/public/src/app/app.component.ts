0fae88130b328895fe2640abad206b961f8607c2
phillip-eth <54546971+phillip-eth@users.noreply.github.com>
2019-11-11 12:33:08 -0500

    hello angular

--- helloAngular/public/src/app/app.component.ts
+++ helloAngular/public/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'public';
   title = 'public';
+  constructor(private _httpService: HttpService) {}
 }
